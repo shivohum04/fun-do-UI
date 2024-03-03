5ba89792e0ce726316a2e683a4843b4f28546dbf
shivohum04 <shivohum04@gmail.com>
2024-03-03 12:07:28 +0530

    [shiv]made some changes

--- src/components/displaynotegrid.js
+++ src/components/displaynotegrid.js
@@ -1,8 +1,21 @@
 import React from 'react'
 import './styles/note-text.css';
 import Notedisplaycard from './notedisplaycard';
-
+import axios from 'axios';
 export default function Displaynotegrid() {
+
+  const requestGetNotesURL = "https://fundoonotes.incubation.bridgelabz.com/api/notes/getNotesList"
+
+      axios.get(requestGetNotesURL)
+          .then(function (response) {
+              console.log(response);
+              
+          })
+          .catch(function (error) {
+              console.log(error);
+              alert('wrong email or password')
+              
+          });
   return (
     <div className='note-grid-main'>
         <Notedisplaycard/>
@@ -14,7 +27,6 @@ export default function Displaynotegrid() {
         <Notedisplaycard/>
         <Notedisplaycard/>
         <Notedisplaycard/>
-
         <Notedisplaycard/>
         <Notedisplaycard/>
         <Notedisplaycard/>
--- src/components/notes.js
+++ src/components/notes.js
@@ -2,19 +2,11 @@ import React, { useState } from 'react';
 import BasicTextFields from './note-text';
 import ExpandingTextArea from './open-note';
 import './styles/note-text.css';
-import axios from 'axios';
+
 import Displaynotegrid from './displaynotegrid';
 export default function Notes() {
   const [showBasicTextFields, setShowBasicTextFields] = useState(true);
-  const getNotesURL="https://fundoonotes.incubation.bridgelabz.com/api/notes/getNotesList"
-  axios.post(getNotesURL)
-          .then(function (response) {
-              console.log(response);   
-          })
-          .catch(function (error) {
-              console.log(error);
-              //alert('get notes failed')
-          });
+
   const handleClick = () => {
     setShowBasicTextFields(!showBasicTextFields);
   };
